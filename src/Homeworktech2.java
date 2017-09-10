e6d8a38de4619b4aeb5cda61d1141a952ce67a9e
zaalovich <zaalovich@icloud.com>
2017-09-10 22:11:50 +0300

    Домашнее задания 2 финальный вариант

--- src/Homeworktech2.java
+++ src/Homeworktech2.java
@@ -8,12 +8,18 @@ public class Homeworktech2 {
         System.out.println(("Спасибо " + line)  +";) Укажите город проживания?" );
         String line1 = in.nextLine();
         System.out.println(("Спасибо " + line)  +";) Укажите пожалуйста Ваш возраст?");
-        String adress = in.nextLine();
+        String old = in.nextLine();
         System.out.println(("Спасибо " + line)  +";) Укажите пожалуйста Ваш адрес?");
         String line2 = in.nextLine();
         System.out.println(("Отлично " + line)  +";) давайте проверим информацию:\n"  );
-        System.out.printf(" Имя:     %s \n Город:   %s \n Возраст: %s лет \n Адрес:   %s \n ", line , line1 , adress , line2 );
-        System.out.println("-------------------------------------------------");
+        System.out.printf(" Имя:     %s \n Город:   %s \n Возраст: %s лет \n Адрес:   %s \n ", line , line1 , old , line2 );
+        System.out.println("-------------------------------------------------"); // первая часть задания
+        System.out.println("Участника опроса зовут " + line + ", живёт в городе - " + line1 + ". Его  возраст " + old + " лет, проживает по адресу " + line2 );
+        System.out.println("-------------------------------------------------"); // вторая часть задания
+        System.out.println(line + " - Имя" );
+        System.out.println(line1  + " - Город");
+        System.out.println(old  + "лет - Возраст");
+        System.out.println(line2  + " - Адрес" );
 
     }
 }
